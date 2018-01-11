8c5e9e823b23b808bd20e1e50f4bc0acef3dc34e
sdarakchiev <s.darakchiev@gmail.com>
2018-01-11 13:29:58 +0200

    ArticleController unit tests

--- Sentio/Sentio.Tests/Web/Controllers/ArticleControllerTests/AllArticles_Should.cs
+++ Sentio/Sentio.Tests/Web/Controllers/ArticleControllerTests/AllArticles_Should.cs
@@ -3,12 +3,15 @@ using Moq;
 using Sentio.Areas.Admin.Models;
 using Sentio.Areas.Admin.Services;
 using Sentio.Controllers;
+using Sentio.Data.DataModels;
+using System.Data.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TestStack.FluentMVCTesting;
+using EntityFramework.Testing;
 
 namespace Sentio.Tests.Web.Controllers.ArticleControllerTests
 {
@@ -21,13 +24,21 @@ namespace Sentio.Tests.Web.Controllers.ArticleControllerTests
             // Arrange
             var articleServiceMock = new Mock<IArticleServices>();
 
-            var model = new List<ArticleViewModel>();
+            List<Article> articles = new List<Article>()
+            {
+                new Article() {Id = 1}
+            };
+
+            articleServiceMock.Setup(m => m.ListAllArticles()).Returns(articles);
+
+            //var model = new List<ArticleViewModel>();
             var controller = new ArticleController(articleServiceMock.Object);
 
             // Act & Assert
             controller
                 .WithCallTo(c => c.AllArticles())
-                .ShouldRenderDefaultView().WithModel<IEnumerable<ArticleViewModel>>();
+                .ShouldRenderDefaultView()
+                .WithModel<IEnumerable<ArticleViewModel>>(m => m.First(a => a.Id == 1));
         }
     }
 }
