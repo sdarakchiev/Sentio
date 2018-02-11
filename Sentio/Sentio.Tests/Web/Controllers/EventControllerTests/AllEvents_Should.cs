3e39af9e782021c03f384515ded22dbdd38b165a
sdarakchiev <s.darakchiev@gmail.com>
2018-02-11 21:43:59 +0200

    EventControllerTests

---
+++ Sentio/Sentio.Tests/Web/Controllers/EventControllerTests/AllEvents_Should.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sentio.Controllers;
+using Sentio.Data.DataModels;
+using Sentio.Data.ViewModels;
+using Sentio.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace Sentio.Tests.Web.Controllers.EventControllerTests
+{
+    [TestClass]
+    public class AllEvents_Should
+    {
+        [TestMethod]
+        public void ReturnDefaultView_WithCorrectModel()
+        {
+            // Arrange
+            var eventServiceMock = new Mock<IEventService>();
+
+            List<Event> events = new List<Event>()
+            {
+                new Event() {Id = 1}
+            };
+
+            var eventsSetMock = new Mock<DbSet<Event>>().SetupData(events);
+            eventServiceMock.Setup(m => m.AllEvents()).Returns(eventsSetMock.Object);
+
+            //var model = articles.Select(a => new ArticleViewModel() { Id = 1 });
+            var controller = new EventController(eventServiceMock.Object);
+
+            // Act & Assert
+            controller
+                .WithCallTo(c => c.AllEvents())
+                .ShouldRenderDefaultView()
+                .WithModel<IEnumerable<EventViewModel>>(m => m.First(a => a.Id == 1));
+        }
+    }
+}
---
+++ Sentio/Sentio.Tests/Web/Controllers/EventControllerTests/EventDetails_Should.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sentio.Controllers;
+using Sentio.Data.DataModels;
+using Sentio.Data.ViewModels;
+using Sentio.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace Sentio.Tests.Web.Controllers.EventControllerTests
+{
+    [TestClass]
+    public class EventDetails_Should
+    {
+        [TestMethod]
+        public void ReturnDefaultView_WithCorrectModel()
+        {
+            //Arrange
+            var eventServiceMock = new Mock<IEventService>();
+            int eventId = 1;
+            string name = "name";
+            string desription = "description";
+            var newEvent = new Event()
+            {
+                Id = eventId,
+                Name = name,
+                Description = desription
+            };
+
+            eventServiceMock.Setup(m => m.GetEvent(eventId)).Returns(newEvent);
+            var controller = new EventController(eventServiceMock.Object);
+
+            // Act & Assert
+            controller
+                .WithCallTo(c => c.EventDetails(eventId))
+                .ShouldRenderDefaultView()
+                .WithModel<EventViewModel>(m => m.Id == eventId);
+        }
+    }
+}
