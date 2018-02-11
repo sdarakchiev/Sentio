b6175ed3130f635d9eaa16d0999299b610c3e147
sdarakchiev <s.darakchiev@gmail.com>
2018-02-11 19:42:22 +0200

    Comments Unit Tests

---
+++ Sentio/Sentio.Tests/Web/Services/ArticleServicesTests/AddComment_Should.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sentio.Areas.Admin.Services;
+using Sentio.Data.DataModels;
+using Sentio.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentio.Tests.Web.Services.ArticleServicesTests
+{
+    [TestClass]
+    public class AddComment_Should
+    {
+        [TestMethod]
+        public void CreateCommentForTheCorrectArticle_WhenParametersAreCorrect()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ApplicationDbContext>();
+            List<Comment> comments = new List<Comment>();
+            int articleId = 1;
+            string content = "content";
+            List<Article> articles = new List<Article>()
+            {
+                new Article()
+                {
+                    Id = articleId,
+                    Comments = comments
+                }
+            };
+            
+            var articlesSetMock = new Mock<DbSet<Article>>().SetupData(articles);
+
+            dbContextMock.SetupGet(m => m.Articles).Returns(articlesSetMock.Object);
+            ArticleServices service = new ArticleServices(dbContextMock.Object);
+
+            //Act
+            service.AddComment(articleId, content);
+
+            //Assert
+            var article = dbContextMock.Object.Articles.First(a => a.Id == articleId);
+            var comment = article.Comments.Single();
+
+            Assert.AreEqual(articleId, comment.ArticleId);
+            Assert.AreEqual(content, comment.Content);
+
+            dbContextMock.Verify(m => m.SaveChanges(), Times.Once);
+
+        }
+    }
+}
---
+++ Sentio/Sentio.Tests/Web/Services/ArticleServicesTests/AllComments_Should.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sentio.Areas.Admin.Services;
+using Sentio.Data.DataModels;
+using Sentio.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentio.Tests.Web.Services.ArticleServicesTests
+{
+    [TestClass]
+    public class AllComments_Should
+    {
+        [TestMethod]
+        public void ReturnAllCommentsForArticle_WhenParameterIsCorrect()
+        {
+            // Arrange
+            var dbContextMock = new Mock<ApplicationDbContext>();
+
+            var articleId = 1;
+            var comment = new Comment();
+            var comments = new List<Comment>() { comment };
+            var article = new Article()
+            {
+                Id = articleId,
+                Comments = comments
+            };
+            var articles = new List<Article>() { article };
+
+            var articlesSetMock = new Mock<DbSet<Article>>();
+            articlesSetMock.SetupData(articles);
+
+            dbContextMock.Setup(m => m.Articles).Returns(articlesSetMock.Object);
+
+            var service = new ArticleServices(dbContextMock.Object);
+
+            // Act
+            var result = service.AllComments(articleId).ToList();
+
+            // Assert
+            CollectionAssert.AreEqual(comments, result);
+        }
+    }
+}
